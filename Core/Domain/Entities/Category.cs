55d3d83ea5ecb690ef41985fbae98a41e6c2d12d
Abdo Othman <abdoetman010@gmail.com>
2026-03-14 15:21:17 +0200

    [ADD] products navigation property for Category model

--- Core/Domain/Entities/Category.cs
+++ Core/Domain/Entities/Category.cs
@@ -1,9 +1,10 @@
 using Core.Domain.Entities.Generics;
 
-namespace Core.Domain.Models;
+namespace Core.Domain.Entities;
 
 public class Category : IdModel
 {
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
+    public ICollection<Product> Products { get; set;} = [];
 }
