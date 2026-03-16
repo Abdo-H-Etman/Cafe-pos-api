6c8114f8193ad75f401390ea6b773f9f7dd93dc7
Abdo Othman <abdoetman010@gmail.com>
2026-03-16 14:20:38 +0200

    [ADD] navigation property to fetch Orders from Branch

--- Core/Domain/Entities/Branch.cs
+++ Core/Domain/Entities/Branch.cs
@@ -9,4 +9,5 @@ public class Branch : IdModel
     public ICollection<User> Users { get; set; } = [];
     public ICollection<BranchProduct> BranchProducts { get; set; } = [];
     public ICollection<StockMovement> StockMovements { get; set; } = [];
+    public ICollection<Order> Orders { get; set; } = [];
 }
