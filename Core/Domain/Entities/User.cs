e600a62cdf11e9c7c48d01d10ae5fe3acc374710
Abdo Othman <abdoetman010@gmail.com>
2026-03-30 23:26:30 +0200

    [ADD] LastLoginAt to user

--- Core/Domain/Entities/User.cs
+++ Core/Domain/Entities/User.cs
@@ -7,6 +7,6 @@ public class User : IdentityUser<Guid>
     public string Name { get; set; } = null!;
     public Guid BranchId { get; set; }
     public DateTime DateJoined { get; set; }
-
+    public DateTime LastLoginAt { get; set; }
     public Branch Branch { get; set; } = null!;
 }
