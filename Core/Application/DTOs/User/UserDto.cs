372c6ea6f60a3578fd77b6d68861fc2a8f271729
Abdo Othman <abdoetman010@gmail.com>
2026-04-04 15:15:50 +0200

    [ADD] BranchId to UserDto so we can use it in RBAC, and make email nullable as it's not required

--- Core/Application/DTOs/User/UserDto.cs
+++ Core/Application/DTOs/User/UserDto.cs
@@ -4,7 +4,8 @@ public record UserDto
 {
     public Guid Id { get; init; }
     public string Name { get; init; } = null!;
-    public string Email { get; init; } = null!;
+    public string? Email { get; init; }
     public string UserName { get; init; } = null!;
+    public Guid BranchId { get; init; }
     public DateTime DateJoined { get; init; }
 }
