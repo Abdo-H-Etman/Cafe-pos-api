cabbf50da4816f271fa3fdc77006de4ee8e8f2b2
Abdo Othman <abdoetman010@gmail.com>
2026-04-04 14:55:07 +0200

    [FIX] Role Based Access Control(RBAC) and add Change password to auth service then to auth controller

--- API/Controllers/AthenticationController.cs
+++ API/Controllers/AthenticationController.cs
@@ -1,7 +1,9 @@
 using System.Security.Claims;
+using Application.Common;
 using Application.DTOs.Auth;
 using Application.DTOs.User;
 using Application.Interfaces.Auth;
+using Core.Domain.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,11 +15,18 @@ public class AuthenticationController : ControllerBase
 {
     private readonly IAuthenticationService _authenticationService;
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IRepositoryManager _repositoryManager;
 
-    public AuthenticationController(IAuthenticationService authenticationService, IHttpContextAccessor httpContextAccessor)
+    public AuthenticationController(IAuthenticationService authenticationService,
+                    IHttpContextAccessor httpContextAccessor,
+                    ICurrentUserService currentUserService,
+                    IRepositoryManager repositoryManager)
     {
         _authenticationService = authenticationService;
         _httpContextAccessor = httpContextAccessor;
+        _currentUserService = currentUserService;
+        _repositoryManager = repositoryManager;
     }
 
     [HttpPost("login")]
@@ -34,11 +43,14 @@ public class AuthenticationController : ControllerBase
     }
 
     [HttpPost("register")]
-    [Authorize(Roles = "Admin, Manager")]
+    [Authorize(Roles = "Admin,Manager")]
     public async Task<IActionResult> Register([FromBody] CreateUserDto request)
     {
         var ipAddress = GetIpAddress();
         var userAgent = GetUserAgent();
+
+        if(!_currentUserService.IsAdmin() && request.BranchId != _currentUserService.BranchId)
+            return Forbid();
 
         var result = await _authenticationService.RegisterUserAsync(request, ipAddress, userAgent);
 
@@ -53,6 +65,23 @@ public class AuthenticationController : ControllerBase
     {
         var userId = GetCurrentUserId();
         var result = await _authenticationService.LogoutAsync(userId);
+
+        if (!result.IsSuccess)
+            return BadRequest(result);
+
+        return Ok(result);
+    }
+
+    [HttpPost("{userId:guid}/change-password")]
+    [Authorize(Roles = "Admin,Manager")]
+    public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordDto request, Guid userId)
+    {
+        var user = await _repositoryManager.User.GetUserByIdAsync(userId);
+
+        if(!_currentUserService.IsAdmin() && user?.BranchId != _currentUserService.BranchId)
+            return Forbid();
+
+        var result = await _authenticationService.ChangePasswordAsync(userId, request);
 
         if (!result.IsSuccess)
             return BadRequest(result);
