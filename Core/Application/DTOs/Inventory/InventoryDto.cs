977dfbe9e97c3cfe0221f1e61dceb19d343e4883
Abdo Othman <abdoetman010@gmail.com>
2026-04-19 17:00:32 +0200

    [ADD] InventoryDto

--- Core/Application/DTOs/Inventory/InventoryDto.cs
+++ Core/Application/DTOs/Inventory/InventoryDto.cs
@@ -2,5 +2,11 @@ namespace Core.Application.DTOs.Inventory;
 
 public record InventoryDto
 {
+    public Guid Id { get; init; }
+    public Guid IngredientId { get; init; }
+    public string IngredientName { get; init; } = null!;
     public decimal CurrentStock { get; init; }
+    public decimal MinStock { get; init; }
+    public string Unit { get; init; } = null!;
+    public bool IsLowStock { get; init; }
 }
