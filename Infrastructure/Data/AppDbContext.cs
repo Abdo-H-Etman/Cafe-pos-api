33ad63dc9cd958a15dc7dbceae950483e2b52e2c
Abdo Othman <abdoetman010@gmail.com>
2026-07-07 00:13:21 +0300

    [ADD] Add global query filter in AppDbContext for Table entity

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -18,21 +18,21 @@ public class AppDbContext : IdentityDbContext<User, Role, Guid, IdentityUserClai
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.Ignore<IdModel>();
-        
+
         base.OnModelCreating(builder);
-        
+
         builder.Ignore<IdentityUserLogin<Guid>>();
         builder.Ignore<IdentityUserToken<Guid>>();
         builder.Ignore<IdentityUserClaim<Guid>>();
         builder.Ignore<IdentityRoleClaim<Guid>>();
-        
+
         builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
-        
+
         builder.Entity<Role>(entity =>
         {
             entity.ToTable("Roles");
         });
-        
+
         builder.Entity<UserRole>(entity =>
         {
             entity.ToTable("UserRoles");
@@ -46,9 +46,10 @@ public class AppDbContext : IdentityDbContext<User, Role, Guid, IdentityUserClai
                 .HasForeignKey(ur => ur.RoleId)
                 .OnDelete(DeleteBehavior.Cascade);
         });
-    
+
         builder.Entity<Inventory>().HasQueryFilter(i => _currentUserService.IsAdmin() || i.BranchId == currentBranchId);
         builder.Entity<StockMovement>().HasQueryFilter(i => _currentUserService.IsAdmin() || i.BranchId == currentBranchId);
-        builder.Entity<User>().HasQueryFilter(u => !_currentUserService.IsAuthenticated || _currentUserService.IsAdmin() ||( u.BranchId == currentBranchId && u.Roles.Any(ur => ur.Role.Name != "Admin")) );
+        builder.Entity<Table>().HasQueryFilter(t => _currentUserService.IsAdmin() || t.BranchId == currentBranchId);
+        builder.Entity<User>().HasQueryFilter(u => !_currentUserService.IsAuthenticated || _currentUserService.IsAdmin() || (u.BranchId == currentBranchId && u.Roles.Any(ur => ur.Role.Name != "Admin")));
     }
 }
