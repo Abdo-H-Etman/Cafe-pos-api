029fae51256516cc76f457c08ce4aaf90e4ac76c
Abdo Othman <abdoetman010@gmail.com>
2026-07-18 16:37:59 +0300

    [ADD] Status field to TableDto to follow the changes in Table model

--- Core/Application/DTOs/Table/TableDto.cs
+++ Core/Application/DTOs/Table/TableDto.cs
@@ -5,4 +5,5 @@ public record TableDto
     public Guid Id { get; init; }
     public string Name { get; init; } = string.Empty;
     public int Capacity { get; init; }
+    public string Status { get; init; } = string.Empty;
 }
