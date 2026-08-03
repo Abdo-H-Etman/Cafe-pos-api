f27c7f6911b8098e72a85396258967a8179b3d34
Abdo Othman <abdoetman010@gmail.com>
2026-08-03 18:25:51 +0300

    [ADD] Global query filter to filter by branch on Reservation entity

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -50,6 +50,7 @@ public class AppDbContext : IdentityDbContext<User, Role, Guid, IdentityUserClai
         builder.Entity<Inventory>().HasQueryFilter(i => _currentUserService.IsAdmin() || i.BranchId == currentBranchId);
         builder.Entity<StockMovement>().HasQueryFilter(i => _currentUserService.IsAdmin() || i.BranchId == currentBranchId);
         builder.Entity<Table>().HasQueryFilter(t => _currentUserService.IsAdmin() || t.BranchId == currentBranchId);
+        builder.Entity<Reservation>().HasQueryFilter(r => _currentUserService.IsAdmin() || r.BranchId == currentBranchId);
         builder.Entity<User>().HasQueryFilter(u => !_currentUserService.IsAuthenticated || _currentUserService.IsAdmin() || (u.BranchId == currentBranchId && u.Roles.Any(ur => ur.Role.Name != "Admin")));
     }
 }
